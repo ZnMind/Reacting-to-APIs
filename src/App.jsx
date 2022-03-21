e3e68a6a606674d6192b6b6d24589abbd445e468
Dan Mann <damann1072@gmail.com>
2022-03-21 10:24:29 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+const App = () => {
+
+    
+
+
+    /* fetch('https://ghibliapi.herokuapp.com/films/58611129-2dbc-4a81-a72f-77ddfc1b1b49')
+    .then(response => {
+        console.log(response);
+        return response.json();
+    })
+    .then(allPosts => {
+        console.log(allPosts);
+    });
+
+    fetch('https://jsonplaceholder.typicode.com/posts/', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newPostObj)
+})
+    .then(response => {
+        console.log(response);
+        return response.json();
+    })
+    .then(serverMsg => {
+        console.log(serverMsg);
+    }); */
+
+    return (
+        <h1>Howdy</h1>
+    )
+
+}
+
+export default App;
